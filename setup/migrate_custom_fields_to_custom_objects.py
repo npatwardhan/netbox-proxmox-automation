5a99c17eee079206b9b5f6c99c5b6cc441ee45e3
Nathan Patwardhan <noopy.org@gmail.com>
2026-03-22 15:34:03 -0400

    initial

---
+++ setup/migrate_custom_fields_to_custom_objects.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+
+import os, sys
+import re
+import pprint
+import yaml
+import urllib3
+import pynetbox
+
+from helpers.netbox_objects import __netbox_make_slug
+
+from pynetbox.core.query import RequestError
+import requests.exceptions
+
+
+class NetBoxAPI:
+    def __init__(self, http_proto: str, netbox_host: str, netbox_port: str, netbox_api_token: str):
+        try:
+            nb_url = f"{http_proto}://{netbox_host}:{netbox_port}"
+            nb_conn = pynetbox.api(url=nb_url, token=netbox_api_token)
+            self.nb_conn = nb_conn
+
+            self.nb_conn.http_session.verify = False
+            urllib3.disable_warnings()
+
+        except requests.exceptions.ConnectionError as e:
+            raise ValueError(f"connection error {e}")
+        except RequestError as e:
+            raise ValueError(f"Request error: {e} ||| {e.status_code} ||| {e.full_response}")
+        except Exception as e:
+            # Catch any other unexpected exceptions
+            print(f"An unexpected error occurred: {type(e).__name__}")
+            print(f"Error details: {e}")            
+
+
+class NetBoxVirtualMachines(NetBoxAPI):
+    def __init__(self, http_proto: str, netbox_host: str, netbox_port: str, netbox_api_token: str):
+        super().__init__(http_proto, netbox_host, netbox_port, netbox_api_token)
+
+        self.collected_vms = {}
+        self.collected_lxc = {}
+
+        self.__collect_vms()
+
+
+    def __collect_vms(self):
+        vm_all = self.nb_conn.virtualization.virtual_machines.all()
+
+        for vm in vm_all:
+            #print("VM", dict(vm))
+            if 'proxmox_vm_type' in vm['custom_fields'] and vm['custom_fields']['proxmox_vm_type'] == 'vm':
+                #print(f"VM INFO: {dict(vm)} {vm['name']} ||| {vm['custom_fields']}")
+                if not vm['name'] in self.collected_vms:
+                    self.collected_vms[vm['name']] = {}
+
+                self.collected_vms[vm['name']] = vm['custom_fields']
+
+                self.collected_vms[vm['name']]['vm_name'] = vm['id']
+                self.collected_vms[vm['name']]['proxmox_discovered_vm'] = False
+
+                if 'tags' in dict(vm):
+                    find_discovered_vm = list(filter(lambda pm_vm_disc: pm_vm_disc['name'] == 'proxmox-vm-discovered', vm['tags']))
+
+                    if len(find_discovered_vm) == 1:
+                        if 'name' in find_discovered_vm[0] and find_discovered_vm[0]['name'] == 'proxmox-vm-discovered':
+                            self.collected_vms[vm['name']]['proxmox_discovered_vm'] = True
+
+                del self.collected_vms[vm['name']]['proxmox_vm_type']
+                del self.collected_vms[vm['name']]['proxmox_node']
+
+    
+class NetBoxVirtualMachinesCustomObjects(NetBoxAPI):
+    def __init__(self, http_proto: str, netbox_host: str, netbox_port: str, netbox_api_token: str):
+        super().__init__(http_proto, netbox_host, netbox_port, netbox_api_token)
+
+
+    def create_nb_vm_co(self, payload: dict):
+        nb_obj = self.nb_conn.plugins.custom_objects.proxmox_vms.create(**payload)
+        return dict(nb_obj)['id']
+
+
+class NetBoxCustomObjectsAndFields(NetBoxAPI):
+    def __init__(self, http_proto: str, netbox_host: str, netbox_port: str, netbox_api_token: str):
+        super().__init__(http_proto, netbox_host, netbox_port, netbox_api_token)
+
+        self.co_id_mappings = {}
+        self.__collect_nb_custom_objects()
+
+    
+    def __collect_nb_custom_objects(self):
+        for cot in self.nb_conn.plugins.custom_objects.custom_object_types.all():
+            cot_info = dict(cot)
+
+            if not 'name' in cot_info:
+                raise ValueError(f"Missing 'name' in {cot}")
+
+            if not 'id' in cot_info:
+                raise ValueError(f"Missing 'id' in {cot}")
+            
+            self.co_id_mappings[cot_info['name']] = cot_info['id']
+
+
+    def nb_create_custom_object(self, co_name: str, co_slug: str):
+        if not co_name in self.co_id_mappings:
+            nb_obj = self.nb_conn.plugins.custom_objects.custom_object_types.create(
+                name = co_name,
+                #verbose_name = co_name,
+                #verbose_name_plural = f"{co_name}s",
+                slug = co_slug
+            )
+            nb_obj_dict = dict(nb_obj)
+
+            self.co_id_mappings[nb_obj_dict['name']] = nb_obj_dict['id']
+
+
+    def nb_create_custom_object_field(self, co_field_data: dict):
+        self.nb_conn.plugins.custom_objects.custom_object_type_fields.create(co_field_data)
+
+
+def main():
+    netbox_cfg = 'netbox.yml'
+
+    with open(netbox_cfg) as nb_f:
+        nb_f = yaml.safe_load(nb_f)
+
+    #print(nb_f)
+
+    nb_obj = NetBoxAPI(nb_f['netbox_http_proto'], nb_f['netbox_host'], nb_f['netbox_port'], nb_f['netbox_api_token'])
+
+    nb_vm_obj = NetBoxVirtualMachines(nb_f['netbox_http_proto'], nb_f['netbox_host'], nb_f['netbox_port'], nb_f['netbox_api_token'])
+
+    print(f"HEY {nb_vm_obj.collected_vms}")
+
+    for collected_vm in nb_vm_obj.collected_vms:
+        nb_migrate_vm_co = NetBoxVirtualMachinesCustomObjects(nb_f['netbox_http_proto'], nb_f['netbox_host'], nb_f['netbox_port'], nb_f['netbox_api_token'])
+        nb_migrate_vm_co.create_nb_vm_co(nb_vm_obj.collected_vms[collected_vm])
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
